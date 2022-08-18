67b97a574cee3458688244386b2ede188f9ab661
Kyle Kelly <kdkel237@learner.dvusd.org>
2022-08-18 07:42:02 -0700

    working

---
+++ VB/Dexs/Dexs/Form1.Designer.vb
@@ -0,0 +1,351 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Dolphin = New System.Windows.Forms.Button()
+        Me.Displaypicturebox = New System.Windows.Forms.PictureBox()
+        Me.Shark = New System.Windows.Forms.Button()
+        Me.Giraffe = New System.Windows.Forms.Button()
+        Me.Sloth = New System.Windows.Forms.Button()
+        Me.Lion = New System.Windows.Forms.Button()
+        Me.SharkPictureBox = New System.Windows.Forms.PictureBox()
+        Me.SlothPictureBox = New System.Windows.Forms.PictureBox()
+        Me.GiraffePictureBox = New System.Windows.Forms.PictureBox()
+        Me.DolphinPictureBox = New System.Windows.Forms.PictureBox()
+        Me.LionPictureBox = New System.Windows.Forms.PictureBox()
+        Me.balls = New System.Windows.Forms.Label()
+        Me.Legs = New System.Windows.Forms.Label()
+        Me.Hairy = New System.Windows.Forms.Label()
+        Me.fhsf = New System.Windows.Forms.Label()
+        Me.FoodChain = New System.Windows.Forms.Label()
+        Me.NameLabel = New System.Windows.Forms.Label()
+        Me.LegsLabel = New System.Windows.Forms.Label()
+        Me.HairyLabel = New System.Windows.Forms.Label()
+        Me.HeightLabel = New System.Windows.Forms.Label()
+        Me.FoodChainLabel = New System.Windows.Forms.Label()
+        Me.Startbutton = New System.Windows.Forms.Button()
+        Me.Restartbutton = New System.Windows.Forms.Button()
+        CType(Me.Displaypicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SharkPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SlothPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GiraffePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DolphinPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LionPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Dolphin
+        '
+        Me.Dolphin.Location = New System.Drawing.Point(4, 12)
+        Me.Dolphin.Name = "Dolphin"
+        Me.Dolphin.Size = New System.Drawing.Size(75, 23)
+        Me.Dolphin.TabIndex = 0
+        Me.Dolphin.Text = "Dolphin "
+        Me.Dolphin.UseVisualStyleBackColor = True
+        Me.Dolphin.Visible = False
+        '
+        'Displaypicturebox
+        '
+        Me.Displaypicturebox.Location = New System.Drawing.Point(4, 65)
+        Me.Displaypicturebox.Name = "Displaypicturebox"
+        Me.Displaypicturebox.Size = New System.Drawing.Size(383, 210)
+        Me.Displaypicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Displaypicturebox.TabIndex = 1
+        Me.Displaypicturebox.TabStop = False
+        '
+        'Shark
+        '
+        Me.Shark.Location = New System.Drawing.Point(85, 12)
+        Me.Shark.Name = "Shark"
+        Me.Shark.Size = New System.Drawing.Size(75, 23)
+        Me.Shark.TabIndex = 2
+        Me.Shark.Text = "Shark"
+        Me.Shark.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        Me.Shark.UseVisualStyleBackColor = True
+        Me.Shark.Visible = False
+        '
+        'Giraffe
+        '
+        Me.Giraffe.Location = New System.Drawing.Point(166, 12)
+        Me.Giraffe.Name = "Giraffe"
+        Me.Giraffe.Size = New System.Drawing.Size(75, 23)
+        Me.Giraffe.TabIndex = 3
+        Me.Giraffe.Text = "Giraffe"
+        Me.Giraffe.UseVisualStyleBackColor = True
+        Me.Giraffe.Visible = False
+        '
+        'Sloth
+        '
+        Me.Sloth.Location = New System.Drawing.Point(247, 12)
+        Me.Sloth.Name = "Sloth"
+        Me.Sloth.Size = New System.Drawing.Size(75, 23)
+        Me.Sloth.TabIndex = 4
+        Me.Sloth.Text = "Sloth "
+        Me.Sloth.UseVisualStyleBackColor = True
+        Me.Sloth.Visible = False
+        '
+        'Lion
+        '
+        Me.Lion.Location = New System.Drawing.Point(328, 12)
+        Me.Lion.Name = "Lion"
+        Me.Lion.Size = New System.Drawing.Size(75, 23)
+        Me.Lion.TabIndex = 5
+        Me.Lion.Text = "Lion"
+        Me.Lion.UseVisualStyleBackColor = True
+        Me.Lion.Visible = False
+        '
+        'SharkPictureBox
+        '
+        Me.SharkPictureBox.Image = CType(resources.GetObject("SharkPictureBox.Image"), System.Drawing.Image)
+        Me.SharkPictureBox.Location = New System.Drawing.Point(118, 596)
+        Me.SharkPictureBox.Name = "SharkPictureBox"
+        Me.SharkPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.SharkPictureBox.TabIndex = 6
+        Me.SharkPictureBox.TabStop = False
+        Me.SharkPictureBox.Visible = False
+        '
+        'SlothPictureBox
+        '
+        Me.SlothPictureBox.Image = CType(resources.GetObject("SlothPictureBox.Image"), System.Drawing.Image)
+        Me.SlothPictureBox.Location = New System.Drawing.Point(330, 596)
+        Me.SlothPictureBox.Name = "SlothPictureBox"
+        Me.SlothPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.SlothPictureBox.TabIndex = 7
+        Me.SlothPictureBox.TabStop = False
+        Me.SlothPictureBox.Visible = False
+        '
+        'GiraffePictureBox
+        '
+        Me.GiraffePictureBox.Image = CType(resources.GetObject("GiraffePictureBox.Image"), System.Drawing.Image)
+        Me.GiraffePictureBox.Location = New System.Drawing.Point(224, 596)
+        Me.GiraffePictureBox.Name = "GiraffePictureBox"
+        Me.GiraffePictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.GiraffePictureBox.TabIndex = 8
+        Me.GiraffePictureBox.TabStop = False
+        Me.GiraffePictureBox.Visible = False
+        '
+        'DolphinPictureBox
+        '
+        Me.DolphinPictureBox.Image = CType(resources.GetObject("DolphinPictureBox.Image"), System.Drawing.Image)
+        Me.DolphinPictureBox.Location = New System.Drawing.Point(12, 596)
+        Me.DolphinPictureBox.Name = "DolphinPictureBox"
+        Me.DolphinPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.DolphinPictureBox.TabIndex = 9
+        Me.DolphinPictureBox.TabStop = False
+        Me.DolphinPictureBox.Visible = False
+        '
+        'LionPictureBox
+        '
+        Me.LionPictureBox.Image = CType(resources.GetObject("LionPictureBox.Image"), System.Drawing.Image)
+        Me.LionPictureBox.Location = New System.Drawing.Point(436, 596)
+        Me.LionPictureBox.Name = "LionPictureBox"
+        Me.LionPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.LionPictureBox.TabIndex = 10
+        Me.LionPictureBox.TabStop = False
+        Me.LionPictureBox.Visible = False
+        '
+        'balls
+        '
+        Me.balls.AutoSize = True
+        Me.balls.Location = New System.Drawing.Point(393, 65)
+        Me.balls.Name = "balls"
+        Me.balls.Size = New System.Drawing.Size(47, 16)
+        Me.balls.TabIndex = 11
+        Me.balls.Text = "Name:"
+        Me.balls.Visible = False
+        '
+        'Legs
+        '
+        Me.Legs.AutoSize = True
+        Me.Legs.Location = New System.Drawing.Point(393, 114)
+        Me.Legs.Name = "Legs"
+        Me.Legs.Size = New System.Drawing.Size(40, 16)
+        Me.Legs.TabIndex = 12
+        Me.Legs.Text = "Legs:"
+        Me.Legs.Visible = False
+        '
+        'Hairy
+        '
+        Me.Hairy.AutoSize = True
+        Me.Hairy.Location = New System.Drawing.Point(393, 157)
+        Me.Hairy.Name = "Hairy"
+        Me.Hairy.Size = New System.Drawing.Size(42, 16)
+        Me.Hairy.TabIndex = 13
+        Me.Hairy.Text = "Hairy:"
+        Me.Hairy.Visible = False
+        '
+        'fhsf
+        '
+        Me.fhsf.AutoSize = True
+        Me.fhsf.Location = New System.Drawing.Point(393, 202)
+        Me.fhsf.Name = "fhsf"
+        Me.fhsf.Size = New System.Drawing.Size(49, 16)
+        Me.fhsf.TabIndex = 14
+        Me.fhsf.Text = "Height:"
+        Me.fhsf.Visible = False
+        '
+        'FoodChain
+        '
+        Me.FoodChain.AutoSize = True
+        Me.FoodChain.Location = New System.Drawing.Point(393, 248)
+        Me.FoodChain.Name = "FoodChain"
+        Me.FoodChain.Size = New System.Drawing.Size(76, 16)
+        Me.FoodChain.TabIndex = 15
+        Me.FoodChain.Text = "FoodChain:"
+        Me.FoodChain.Visible = False
+        '
+        'NameLabel
+        '
+        Me.NameLabel.AutoSize = True
+        Me.NameLabel.Location = New System.Drawing.Point(482, 65)
+        Me.NameLabel.Name = "NameLabel"
+        Me.NameLabel.Size = New System.Drawing.Size(48, 16)
+        Me.NameLabel.TabIndex = 16
+        Me.NameLabel.Text = "Label6"
+        Me.NameLabel.Visible = False
+        '
+        'LegsLabel
+        '
+        Me.LegsLabel.AutoSize = True
+        Me.LegsLabel.Location = New System.Drawing.Point(480, 114)
+        Me.LegsLabel.Name = "LegsLabel"
+        Me.LegsLabel.Size = New System.Drawing.Size(48, 16)
+        Me.LegsLabel.TabIndex = 17
+        Me.LegsLabel.Text = "Label7"
+        Me.LegsLabel.Visible = False
+        '
+        'HairyLabel
+        '
+        Me.HairyLabel.AutoSize = True
+        Me.HairyLabel.Location = New System.Drawing.Point(482, 157)
+        Me.HairyLabel.Name = "HairyLabel"
+        Me.HairyLabel.Size = New System.Drawing.Size(48, 16)
+        Me.HairyLabel.TabIndex = 18
+        Me.HairyLabel.Text = "Label8"
+        Me.HairyLabel.Visible = False
+        '
+        'HeightLabel
+        '
+        Me.HeightLabel.AutoSize = True
+        Me.HeightLabel.Location = New System.Drawing.Point(480, 202)
+        Me.HeightLabel.Name = "HeightLabel"
+        Me.HeightLabel.Size = New System.Drawing.Size(48, 16)
+        Me.HeightLabel.TabIndex = 19
+        Me.HeightLabel.Text = "Label9"
+        Me.HeightLabel.Visible = False
+        '
+        'FoodChainLabel
+        '
+        Me.FoodChainLabel.AutoSize = True
+        Me.FoodChainLabel.Location = New System.Drawing.Point(475, 248)
+        Me.FoodChainLabel.Name = "FoodChainLabel"
+        Me.FoodChainLabel.Size = New System.Drawing.Size(55, 16)
+        Me.FoodChainLabel.TabIndex = 20
+        Me.FoodChainLabel.Text = "Label10"
+        Me.FoodChainLabel.Visible = False
+        '
+        'Startbutton
+        '
+        Me.Startbutton.Location = New System.Drawing.Point(360, 222)
+        Me.Startbutton.Name = "Startbutton"
+        Me.Startbutton.Size = New System.Drawing.Size(75, 23)
+        Me.Startbutton.TabIndex = 21
+        Me.Startbutton.Text = "Start"
+        Me.Startbutton.UseVisualStyleBackColor = True
+        '
+        'Restartbutton
+        '
+        Me.Restartbutton.Location = New System.Drawing.Point(713, 12)
+        Me.Restartbutton.Name = "Restartbutton"
+        Me.Restartbutton.Size = New System.Drawing.Size(75, 23)
+        Me.Restartbutton.TabIndex = 22
+        Me.Restartbutton.Text = "Restart"
+        Me.Restartbutton.UseVisualStyleBackColor = True
+        Me.Restartbutton.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Thistle
+        Me.ClientSize = New System.Drawing.Size(800, 460)
+        Me.Controls.Add(Me.Restartbutton)
+        Me.Controls.Add(Me.Startbutton)
+        Me.Controls.Add(Me.FoodChainLabel)
+        Me.Controls.Add(Me.HeightLabel)
+        Me.Controls.Add(Me.HairyLabel)
+        Me.Controls.Add(Me.LegsLabel)
+        Me.Controls.Add(Me.NameLabel)
+        Me.Controls.Add(Me.FoodChain)
+        Me.Controls.Add(Me.fhsf)
+        Me.Controls.Add(Me.Hairy)
+        Me.Controls.Add(Me.Legs)
+        Me.Controls.Add(Me.balls)
+        Me.Controls.Add(Me.LionPictureBox)
+        Me.Controls.Add(Me.DolphinPictureBox)
+        Me.Controls.Add(Me.GiraffePictureBox)
+        Me.Controls.Add(Me.SlothPictureBox)
+        Me.Controls.Add(Me.SharkPictureBox)
+        Me.Controls.Add(Me.Lion)
+        Me.Controls.Add(Me.Sloth)
+        Me.Controls.Add(Me.Giraffe)
+        Me.Controls.Add(Me.Shark)
+        Me.Controls.Add(Me.Displaypicturebox)
+        Me.Controls.Add(Me.Dolphin)
+        Me.Name = "Form1"
+        Me.Text = "AnimalDex"
+        CType(Me.Displaypicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SharkPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SlothPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GiraffePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DolphinPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LionPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Dolphin As Button
+    Friend WithEvents Displaypicturebox As PictureBox
+    Friend WithEvents Shark As Button
+    Friend WithEvents Giraffe As Button
+    Friend WithEvents Sloth As Button
+    Friend WithEvents Lion As Button
+    Friend WithEvents SharkPictureBox As PictureBox
+    Friend WithEvents SlothPictureBox As PictureBox
+    Friend WithEvents GiraffePictureBox As PictureBox
+    Friend WithEvents DolphinPictureBox As PictureBox
+    Friend WithEvents balls As Label
+    Friend WithEvents Legs As Label
+    Friend WithEvents Hairy As Label
+    Friend WithEvents fhsf As Label
+    Friend WithEvents FoodChain As Label
+    Friend WithEvents NameLabel As Label
+    Friend WithEvents LegsLabel As Label
+    Friend WithEvents HairyLabel As Label
+    Friend WithEvents HeightLabel As Label
+    Friend WithEvents FoodChainLabel As Label
+    Friend WithEvents Startbutton As Button
+    Friend WithEvents Restartbutton As Button
+    Friend WithEvents LionPictureBox As PictureBox
+End Class
