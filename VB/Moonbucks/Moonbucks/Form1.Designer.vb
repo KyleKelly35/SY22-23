27db73d43b428d4c973a01970f1fa78f1774a202
Kyle Kelly <kdkel237@learner.dvusd.org>
2022-09-09 08:06:51 -0700

    MoonBucks

---
+++ VB/Moonbucks/Moonbucks/Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CaluateButton = New System.Windows.Forms.Button()
+        Me.PrintButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.decafTextBox = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.RegularTextBox = New System.Windows.Forms.TextBox()
+        Me.TotalTextBox = New System.Windows.Forms.TextBox()
+        Me.PoundsTextBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.StatesComboBox = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'CaluateButton
+        '
+        Me.CaluateButton.Location = New System.Drawing.Point(919, 283)
+        Me.CaluateButton.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.CaluateButton.Name = "CaluateButton"
+        Me.CaluateButton.Size = New System.Drawing.Size(138, 48)
+        Me.CaluateButton.TabIndex = 0
+        Me.CaluateButton.Text = "&Caluate"
+        Me.CaluateButton.UseVisualStyleBackColor = True
+        '
+        'PrintButton
+        '
+        Me.PrintButton.Location = New System.Drawing.Point(919, 340)
+        Me.PrintButton.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.PrintButton.Name = "PrintButton"
+        Me.PrintButton.Size = New System.Drawing.Size(138, 48)
+        Me.PrintButton.TabIndex = 1
+        Me.PrintButton.Text = "&Print"
+        Me.PrintButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(919, 397)
+        Me.ClearButton.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(138, 48)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear "
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(919, 454)
+        Me.ExitButton.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(138, 48)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "&Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(440, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(127, 24)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Order Form"
+        '
+        'decafTextBox
+        '
+        Me.decafTextBox.Location = New System.Drawing.Point(676, 454)
+        Me.decafTextBox.Name = "decafTextBox"
+        Me.decafTextBox.Size = New System.Drawing.Size(100, 31)
+        Me.decafTextBox.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(52, 292)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(395, 31)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(52, 65)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(395, 31)
+        Me.TextBox3.TabIndex = 7
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(52, 163)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(395, 31)
+        Me.TextBox4.TabIndex = 8
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(157, 454)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 31)
+        Me.TextBox6.TabIndex = 10
+        '
+        'RegularTextBox
+        '
+        Me.RegularTextBox.Location = New System.Drawing.Point(522, 454)
+        Me.RegularTextBox.Name = "RegularTextBox"
+        Me.RegularTextBox.Size = New System.Drawing.Size(100, 31)
+        Me.RegularTextBox.TabIndex = 11
+        '
+        'TotalTextBox
+        '
+        Me.TotalTextBox.Location = New System.Drawing.Point(931, 163)
+        Me.TotalTextBox.Name = "TotalTextBox"
+        Me.TotalTextBox.Size = New System.Drawing.Size(100, 31)
+        Me.TotalTextBox.TabIndex = 12
+        '
+        'PoundsTextBox
+        '
+        Me.PoundsTextBox.Location = New System.Drawing.Point(931, 38)
+        Me.PoundsTextBox.Name = "PoundsTextBox"
+        Me.PoundsTextBox.Size = New System.Drawing.Size(100, 31)
+        Me.PoundsTextBox.TabIndex = 13
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(48, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 24)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "&Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(927, 121)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 24)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "Total price:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(672, 409)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 24)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "&Decaf:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(518, 409)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(90, 24)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "&Regular:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(153, 421)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(48, 24)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "&Zip:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(48, 427)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(63, 24)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "&State:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(48, 254)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(56, 24)
+        Me.Label8.TabIndex = 20
+        Me.Label8.Text = "&City:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(48, 133)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(93, 24)
+        Me.Label9.TabIndex = 21
+        Me.Label9.Text = "&Address:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(927, 9)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(172, 24)
+        Me.Label10.TabIndex = 22
+        Me.Label10.Text = "Pounds Ordered:"
+        '
+        'StatesComboBox
+        '
+        Me.StatesComboBox.AllowDrop = True
+        Me.StatesComboBox.FormattingEnabled = True
+        Me.StatesComboBox.Items.AddRange(New Object() {"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"})
+        Me.StatesComboBox.Location = New System.Drawing.Point(52, 453)
+        Me.StatesComboBox.Name = "StatesComboBox"
+        Me.StatesComboBox.Size = New System.Drawing.Size(59, 32)
+        Me.StatesComboBox.TabIndex = 23
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1300, 675)
+        Me.Controls.Add(Me.StatesComboBox)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PoundsTextBox)
+        Me.Controls.Add(Me.TotalTextBox)
+        Me.Controls.Add(Me.RegularTextBox)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.decafTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.PrintButton)
+        Me.Controls.Add(Me.CaluateButton)
+        Me.Font = New System.Drawing.Font("Imprint MT Shadow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.Name = "Form1"
+        Me.Text = "MoonBucks"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CaluateButton As Button
+    Friend WithEvents PrintButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents decafTextBox As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents RegularTextBox As TextBox
+    Friend WithEvents TotalTextBox As TextBox
+    Friend WithEvents PoundsTextBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents StatesComboBox As ComboBox
+End Class
---
+++ VB/Moonbucks/Moonbucks/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Dim Dpounds As Decimal
+    Dim Rpounds As Decimal
+    Private Sub caluatebutton_Click(sender As Object, e As EventArgs) Handles CaluateButton.Click
+        Decimal.TryParse(decafTextBox.Text, Dpounds)
+        Decimal.TryParse(RegularTextBox.Text, Rpounds)
+        PoundsTextBox.Text = Rpounds + Dpounds
+        TotalTextBox.Text = ((Rpounds + Dpounds) * 4.99).ToString("C2")
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox6.Clear()
+        PoundsTextBox.Clear()
+        TotalTextBox.Clear()
+        decafTextBox.Clear()
+        RegularTextBox.Clear()
+        StatesComboBox.ResetText()
+    End Sub
+End Class
