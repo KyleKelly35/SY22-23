a329e81ff94aa987b92caceab192f865d3634af7
Kyle Kelly <kdkel237@learner.dvusd.org>
2022-09-12 07:58:17 -0700

    Colors

---
+++ VB/Cooloo/Cooloo/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.RedTextBox = New System.Windows.Forms.TextBox()
+        Me.BlueTextBox = New System.Windows.Forms.TextBox()
+        Me.GreenTextBox = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(71, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Red"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(71, 95)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "&Green"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(71, 134)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "&Blue"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(74, 223)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(108, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "&View Color"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(224, 223)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "&Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(341, 73)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'RedTextBox
+        '
+        Me.RedTextBox.Location = New System.Drawing.Point(150, 60)
+        Me.RedTextBox.Name = "RedTextBox"
+        Me.RedTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.RedTextBox.TabIndex = 6
+        '
+        'BlueTextBox
+        '
+        Me.BlueTextBox.Location = New System.Drawing.Point(150, 134)
+        Me.BlueTextBox.Name = "BlueTextBox"
+        Me.BlueTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.BlueTextBox.TabIndex = 7
+        '
+        'GreenTextBox
+        '
+        Me.GreenTextBox.Location = New System.Drawing.Point(150, 95)
+        Me.GreenTextBox.Name = "GreenTextBox"
+        Me.GreenTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.GreenTextBox.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GreenTextBox)
+        Me.Controls.Add(Me.BlueTextBox)
+        Me.Controls.Add(Me.RedTextBox)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Colors"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents RedTextBox As TextBox
+    Friend WithEvents BlueTextBox As TextBox
+    Friend WithEvents GreenTextBox As TextBox
+End Class
---
+++ VB/Cooloo/Cooloo/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Dim inputred As Integer
+    Dim inputgreen As Integer
+    Dim inputblue As Integer
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Integer.TryParse(RedTextBox.Text, inputred)
+        Integer.TryParse(GreenTextBox.Text, inputgreen)
+        Integer.TryParse(BlueTextBox.Text, inputblue)
+        PictureBox1.BackColor = Color.FromArgb(inputred, inputgreen, inputblue)
+    End Sub
+End Class
